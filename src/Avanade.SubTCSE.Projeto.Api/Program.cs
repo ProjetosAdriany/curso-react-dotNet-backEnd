14d0dbb4503e3ee4646a767fd34d0c0605ba98a8
adriany.aires.vieira <adriany.aires.vieira@WINAP6FW1EZRQAC>
2021-06-15 08:59:53 -0300

    Implementation DDD

--- src/Avanade.SubTCSE.Projeto.Api/Program.cs
+++ src/Avanade.SubTCSE.Projeto.Api/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Avanade.SubTCSE.Projeto.Api
+namespace Avanade.SubTCSE.Projeto.API
 {
     public class Program
     {
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,17 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    // Record: garante imutabilidade | A propriedade so pode ser alterada no construtor
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; set; }
+
+        public string RoleName { get; init; }
+
+    }
+}
