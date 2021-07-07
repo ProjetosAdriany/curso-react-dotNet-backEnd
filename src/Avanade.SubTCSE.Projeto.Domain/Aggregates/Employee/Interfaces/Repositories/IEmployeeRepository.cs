f24372f6c76923b5ff6b6686131ccece1e81a6c1
adriany.aires.vieira <adriany.aires.vieira@WINAP6FW1EZRQAC>
2021-07-07 07:36:26 -0300

    Create MongoDBContext

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repositories
 {
-    public interface IEmployeeRepository
+    public interface IEmployeeRepository : IBaseRepository<Entities.Employee, string>
     {
     }
 }
